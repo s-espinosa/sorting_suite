97c068e783a1ce800a2ca5c8778559d1ae2c7252
Sal Espinosa <sespinos@gmail.com>
2016-02-12 21:25:52 -0700

    Remove test that BubbleSort is an Object/add test for numbers

--- test/bubble_sort_test.rb
+++ test/bubble_sort_test.rb
@@ -6,7 +6,6 @@ require_relative '../lib/bubble_sort.rb'
 class BubbleSortTest < Minitest::Test
   def test_it_sorts_data_passed_to_it
     sorter = BubbleSort.new
-    assert_equal true, sorter.is_a?(Object)
 
     actual = sorter.sort(["d", "b", "a", "c"])
     expected = ["a", "b", "c", "d"]
@@ -14,7 +13,11 @@ class BubbleSortTest < Minitest::Test
   end
 
   def test_it_sorts_numbers_passed_to_it
-    skip
+    sorter = BubbleSort.new
+
+    actual = sorter.sort([5, 7, 14, 29, 2])
+    expected = [2, 5, 7, 14, 29]
+    assert_equal expected, actual
   end
 
   def test_it_checks_that_all_items_passed_are_the_same_type
