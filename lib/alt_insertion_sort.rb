2440e7cbc700dff4aff13cce7984743417eebf23
Sal Espinosa <sespinos@gmail.com>
2016-02-15 21:11:53 -0700

    Add not-in-place InsertionSort

---
+++ lib/alt_insertion_sort.rb
@@ -0,0 +1,24 @@
+require 'pry'
+
+class InsertionSort
+  def sort(items)
+    sorted = [items.shift]
+    until items[0] == nil
+      item_to_add = items.shift
+      inserted = false
+      index = sorted.length - 1
+      until inserted == true
+        if item_to_add > sorted[index]
+          sorted.insert(index+1, item_to_add)
+          inserted = true
+        elsif index == 0
+          sorted.unshift(item_to_add)
+          inserted = true
+        else
+          index = index - 1
+        end
+      end
+    end
+    sorted
+  end
+end
